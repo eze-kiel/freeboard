7b3598883c60abeddae44cf51e31b8d8c8afe0df
eze-kiel <ezekiel@fastmail.com>
2020-04-19 21:32:47 +0200

    commented function

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import "net/url"
 
+// IsURL checks if input is an URL
 func IsURL(str string) bool {
 	u, err := url.Parse(str)
 	return err == nil && u.Scheme != "" && u.Host != ""
