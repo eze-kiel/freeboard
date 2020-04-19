d348a172e28a1c2beae56a2c7d6d3395dc931c09
eze-kiel <ezekiel@fastmail.com>
2020-04-19 23:10:40 +0200

    added timeout

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/eze-kiel/freeboard/handlers"
 )
 
 func main() {
 	router := handlers.HandleFunc()
-	log.Fatal(http.ListenAndServe(":8080", router))
+	timeoutRouter := http.TimeoutHandler(router, time.Second*3, "Timeout!")
+	log.Fatal(http.ListenAndServe(":8080", timeoutRouter))
 }
