2161ca12161eb88bcd209848ad81b01559bc06b1
eze-kiel <ezekiel@fastmail.com>
2020-04-23 18:31:48 +0200

    added flag for prod mode

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"flag"
+	"net/http"
+	"time"
+
 	"github.com/caddyserver/certmagic"
 	log "github.com/sirupsen/logrus"
 
@@ -8,22 +12,29 @@ import (
 )
 
 func main() {
-	// srv := &http.Server{
-	// 	Addr:         ":8080",
-	// 	Handler:      handlers.HandleFunc(),
-	// 	ReadTimeout:  5 * time.Second,
-	// 	WriteTimeout: 10 * time.Second,
-	// }
+	var prod bool
+	flag.BoolVar(&prod, "prod", false, "production mode")
+	flag.Parse()
+	switch prod {
+	case true:
+		// read and agree to your CA's legal documents
+		certmagic.DefaultACME.Agreed = true
 
-	// read and agree to your CA's legal documents
-	certmagic.DefaultACME.Agreed = true
+		// provide an email address
+		certmagic.DefaultACME.Email = "hugoblanc@fastmail.com"
 
-	// provide an email address
-	certmagic.DefaultACME.Email = "hugoblanc@fastmail.com"
+		log.Info("[PROD] Server is starting, wish me luck boys")
+		certmagic.HTTPS([]string{"freeboard.tech", "www.freeboard.tech"}, handlers.HandleFunc())
 
-	log.Info("Server is starting, wish me luck boys")
-	// err := certmagic.HTTPS([]string{"example.com", "www.example.com"}, mux)
+	case false:
+		srv := &http.Server{
+			Addr:         ":8080",
+			Handler:      handlers.HandleFunc(),
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+		}
+		log.Info("[DEV] Server is starting, wish me luck boys")
+		log.Println(srv.ListenAndServe())
+	}
 
-	certmagic.HTTPS([]string{"freeboard.tech", "www.freeboard.tech"}, handlers.HandleFunc())
-	// log.Println(srv.ListenAndServe())
 }
